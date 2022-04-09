e09a19b5348b86400caa1d707ed6ff337c2147da
pranav.v <pranav.v@mitsogo.com>
2022-04-09 07:48:55 +0530

    fix(landingPage): handle responsive view

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,6 +6,7 @@ export default class ApplicationController extends Controller {
   @service global;
   @tracked isShowQuestionnairePage = false;
   @tracked isSpinnerActive = true;
+  @tracked isNotDarkMode = true;
 
   clearTimeout = '';
   @action
@@ -16,5 +17,25 @@ export default class ApplicationController extends Controller {
       this.isShowQuestionnairePage = true;
       this.isSpinnerActive = false;
     }, 500);
+  }
+
+  @action
+  domInit() {
+    // check dark theme
+    const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
+    if (darkThemeMq.matches) {
+      this.toggleDarkModeClass();
+      this.isNotDarkMode = false;
+      // Theme set to dark.
+    }
+  }
+
+  @action
+  onUpdateSwitch() {
+    this.toggleDarkModeClass();
+  }
+
+  toggleDarkModeClass() {
+    document.body.classList.toggle('dark-mode');
   }
 }
