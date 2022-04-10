a33f6a4736a4c478e6f456c1f6f4c70f2a867798
pranav.v <pranav.v@mitsogo.com>
2022-04-11 00:18:09 +0530

    feat: add thanks page and fix darkmode issues

--- app/services/global.js
+++ app/services/global.js
@@ -3,4 +3,5 @@ import { tracked } from '@glimmer/tracking';
 export default class GlobalService extends Service {
   @tracked isLandingPageVisited = false;
   @tracked rows = []; // store all questions and meta details
+  @tracked isShowSubmitPage = false;
 }
